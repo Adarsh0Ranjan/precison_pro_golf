187ae12b674cdb1ae4ab5674c3a150bff2214e5e
adarsh <adarsh0ranjan@gmail.com>
2022-09-24 07:37:49 +0530

    Initial Commit

---
+++ precision_pro_golf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  precision_pro_golf
+//
+//  Created by Roro Solutions on 24/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ precision_pro_golf/precision_pro_golfApp.swift
@@ -0,0 +1,17 @@
+//
+//  precision_pro_golfApp.swift
+//  precision_pro_golf
+//
+//  Created by Roro Solutions on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct precision_pro_golfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
